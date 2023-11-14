756face28a7b3817e9bd66b8c42184e2ca2d0ca0
GiselleLop <giselleLop@users.noreply.github.com>
2023-11-14 00:52:18 -0500

    hito 1 terminado, hito 2 a medias, la funcion para hacer la consulta http no funciona, sin embargo los test del hito 1 si pasan

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,26 @@
-const mdLinks = require('../');
-
+const mdLinksModule = require('../index.js');
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return a resolved function', () => {
+    const mdLinksFunction = mdLinksModule().mdLinks('./README.md', true);
+    return mdLinksFunction.then((result) => {
+      expect(result).toEqual(expect.any(Array));
+    });
   });
 
+  it('should handle errors', () => {
+    const mdLinksFunction = mdLinksModule().mdLinks('./hola.md');
+    return mdLinksFunction.catch((error) => {
+      expect(error).toBeDefined();
+      // Agrega más expectativas según lo que estás probando
+    });
+  });
 });
+
+
+// describe('mdLinks', () => {
+//   it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => {
+//     return mdLinks('miArchivo.md').then((result) => {
+//       expect...;
+//     });
+//   });
